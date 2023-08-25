d0a5a22b50607ef2c358ffe9aaf78bc44edf0028
suhailzap <55779498+suhailzap@users.noreply.github.com>
2023-08-25 10:17:06 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,8 @@
 provider "aws" {
   profile = "default"
   region  = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   default_tags {
     tags = {
       env = "example",
